8357ee3326214da3c0eb7f0ff5ee838b0d6c043d
Dennis Kruse <dkruse.ocholt@ewetel.net>
2020-12-12 22:28:16 +0100

    added dashboard item

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import vuetify from "./plugins/vuetify";
 
 import VuetifyDialog from "vuetify-dialog";
 import "vuetify-dialog/dist/vuetify-dialog.css";
+import VueApexCharts from 'vue-apexcharts'
+
+Vue.use(VueApexCharts)
 
 Vue.use(VuetifyDialog, {
   context: {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,15 +26,40 @@
       Welcome to basketball manager. This is an free software, completely
       written in VueJS and VuetifyJS. Feel free and checkout my github repo.
     </v-alert>
+
+    <v-row>
+      <v-col :cols="4">
+        <apexcharts 
+          type="donut" 
+          :options="chartOptions" 
+          :series="series"
+        ></apexcharts>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import VueApexCharts from 'vue-apexcharts'
+
 export default {
-  data() {
-    return {
-      alert: this.$store.state.settings.showWelcomeDialog
-    }
-  }
-};
+    components: {
+      apexcharts: VueApexCharts,
+    },
+    data: function() {
+      return {
+        series: [
+          this.$store.state.clubs[0].players.filter(x => x.position === 'PG').length, 
+          this.$store.state.clubs[0].players.filter(x => x.position === 'SG').length, 
+          this.$store.state.clubs[0].players.filter(x => x.position === 'SF').length, 
+          this.$store.state.clubs[0].players.filter(x => x.position === 'PF').length,
+          this.$store.state.clubs[0].players.filter(x => x.position === 'C').length
+        ],
+        chartOptions: {
+          labels: ['Point Guards', 'Shooting Guards', 'Small Forwards', 'Power Forwards', 'Center'],
+        },
+        alert: this.$store.state.settings.showWelcomeDialog
+      }
+    },
+}
 </script>
